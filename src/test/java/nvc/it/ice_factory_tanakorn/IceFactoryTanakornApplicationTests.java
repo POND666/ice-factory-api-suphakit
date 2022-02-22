27be4ceb63da8f6a9dfe92a4f2f0549bddabf03b
Pond666 <suphakitpyto3321@gmail.com>
2022-02-22 13:31:04 +0700

    ฉันทำอะไร

---
+++ src/test/java/nvc/it/ice_factory_tanakorn/IceFactoryTanakornApplicationTests.java
@@ -0,0 +1,13 @@
+package nvc.it.ice_factory_suphakit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IceFactoryTanakornApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
